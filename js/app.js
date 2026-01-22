44f659336cbba55c47bf959f51c571379b0b9f34
Neitodesu <95891209+Neitodesu@users.noreply.github.com>
2026-01-21 23:01:31 -0500

    finished game loop

--- js/app.js
+++ js/app.js
@@ -1,28 +1,39 @@
+const resetBtn = document.querySelector('#resetBtn');
+const container = document.querySelector('.main-container');
+
 function createPlayer(name, char) {
-  function getChar() {
+  const getChar = () => {
     return char;
-  }
-  return { name, getChar };
+  };
+  const winner = () => {
+    return `${name} Wins!`;
+  };
+  return { name, getChar, winner };
 }
 
 const createBoard = () => {
   const player1 = createPlayer('Nate', 'X');
   const player2 = createPlayer('Amaya', 'O');
+  const status = document.querySelector('.game-status');
   let board = [];
-  let turn = true;
-  let container = document.querySelector('.main-container');
+  let turn = false;
+  let gameOver = false;
+  let num = 9;
 
   const grid = () => {
+    status.textContent = `${player1.name}, your turn!`;
     for (i = 0; i < 9; i++) {
       let div = document.createElement('div');
       div.classList.add('square');
       div.setAttribute('id', i);
       container.appendChild(div);
 
-      board.push(div);
+      board.push(i);
 
       div.addEventListener('click', () => {
-        toggleTurn();
+        if (gameOver) {
+          return;
+        }
         updateGrid(div);
       });
     }
@@ -32,17 +43,109 @@ const createBoard = () => {
     if (!div.textContent == '') {
       return;
     }
-    divIndex = div.getAttribute('id');
-    console.log(divIndex);
-    board[divIndex] = player1.getChar();
-    console.log(board);
-    div.textContent = player1.getChar();
-    // checkWinner(board);
+
+    toggleTurn(turn);
+
+    if (turn) {
+      divIndex = div.getAttribute('id');
+      board[divIndex] = player1.getChar();
+      div.textContent = player1.getChar();
+    }
+
+    if (!turn) {
+      divIndex = div.getAttribute('id');
+      board[divIndex] = player2.getChar();
+      div.textContent = player2.getChar();
+    }
+
+    num--;
+    checkWinner();
   };
 
   const toggleTurn = () => {
-    return;
+    if (turn == true) {
+      turn = false;
+      status.textContent = `${player1.name}, your turn!`;
+    } else {
+      turn = true;
+      status.textContent = `${player2.name}, your turn!`;
+    }
   };
+
+  const checkWinner = () => {
+    if (board[0] == board[1] && board[0] == board[2]) {
+      if (turn == true) {
+        status.textContent = player1.winner();
+      } else {
+        status.textContent = player2.winner();
+      }
+      gameOver = true;
+    }
+    if (board[0] == board[3] && board[0] == board[6]) {
+      if (turn == true) {
+        status.textContent = player1.winner();
+      } else {
+        status.textContent = player2.winner();
+      }
+      gameOver = true;
+    }
+    if (board[0] == board[4] && board[0] == board[8]) {
+      if (turn == true) {
+        status.textContent = player1.winner();
+      } else {
+        status.textContent = player2.winner();
+      }
+      gameOver = true;
+    }
+    if (board[3] == board[4] && board[3] == board[5]) {
+      if (turn == true) {
+        status.textContent = player1.winner();
+      } else {
+        status.textContent = player2.winner();
+      }
+      gameOver = true;
+    }
+    if (board[6] == board[7] && board[6] == board[8]) {
+      if (turn == true) {
+        status.textContent = player1.winner();
+      } else {
+        status.textContent = player2.winner();
+      }
+      gameOver = true;
+    }
+    if (board[1] == board[4] && board[1] == board[7]) {
+      if (turn == true) {
+        status.textContent = player1.winner();
+      } else {
+        status.textContent = player2.winner();
+      }
+      gameOver = true;
+    }
+    if (board[2] == board[5] && board[2] == board[8]) {
+      if (turn == true) {
+        status.textContent = player1.winner();
+      } else {
+        status.textContent = player2.winner();
+      }
+      gameOver = true;
+    }
+    if (board[2] == board[4] && board[2] == board[6]) {
+      if (turn == true) {
+        status.textContent = player1.winner();
+      } else {
+        status.textContent = player2.winner();
+      }
+      gameOver = true;
+    }
+    if (num == 0) {
+      gameOver = true;
+      status.textContent = 'TIE GAME! Hit reset for new game';
+    }
+  };
+
+  resetBtn.addEventListener('click', () => {
+    window.location.reload();
+  });
 
   return { grid };
 };
